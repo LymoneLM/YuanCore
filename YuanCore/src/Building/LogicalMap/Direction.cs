00ff1b3a72bf74a10bd5247f46b720cb65715677
LymoneLM <Lymone@foxmail.com>
2026-03-04 03:36:25 +0800

    feat(Building/LogicalMap): 完善GirdMap 增加边的获取逻辑

---
+++ YuanCore/src/Building/LogicalMap/Direction.cs
@@ -0,0 +1,14 @@
+﻿namespace YuanCore.Building;
+
+/// <summary>
+/// 逻辑网格方向
+/// x 轴正方向 = South
+/// y 轴正方向 = East
+/// </summary>
+public enum Direction
+{
+    North,
+    West,
+    South,
+    East,
+}
--- YuanCore/src/Building/LogicalMap/GirdMap.cs
+++ YuanCore/src/Building/LogicalMap/GirdMap.cs
@@ -3,16 +3,32 @@ using UnityEngine;
 
 namespace YuanCore.Building;
 
-public sealed class GridMap<T1, T2>
+/// <summary>
+/// 逻辑网格封装
+/// x 轴正方向 = South
+/// y 轴正方向 = East
+/// </summary>
+public sealed class GridMap<TCell, TEdge>
+    where TCell : struct
+    where TEdge : struct
 {
-    private T1[,] _cells;
-    private T2[,] _horizontalEdges;
-    private T2[,] _verticalEdges;
+    private readonly TCell[,] _cells;
+
+    // 以 x 为索引的边
+    // 尺寸 [width + 1, height]
+    // cell 的 North / South 边
+    private readonly TEdge[,] _xEdges;
+
+    // 以 y 为索引的边
+    // 尺寸 [width, height + 1]
+    // cell 的 West / East 边
+    private readonly TEdge[,] _yEdges;
 
     public int MinX { get; }
     public int MinY { get; }
     public int Width { get; }
     public int Height { get; }
+
     public int MaxX => MinX + Width - 1;
     public int MaxY => MinY + Height - 1;
 
@@ -25,17 +41,46 @@ public sealed class GridMap<T1, T2>
         MinY = minY;
         Width = width;
         Height = height;
-        _cells = new T1[width, height];
-        _horizontalEdges = new T2[width + 1, height];
-        _verticalEdges = new T2[width, height + 1];
+
+        _cells = new TCell[width, height];
+        _xEdges = new TEdge[width + 1, height];
+        _yEdges = new TEdge[width, height + 1];
     }
 
+
     public bool Contains(int x, int y) => x >= MinX && x <= MaxX && y >= MinY && y <= MaxY;
-    public bool Contains(Vector2Int girdPosition) => Contains(girdPosition.x, girdPosition.y);
+    public bool Contains(Vector2Int gridPosition) => Contains(gridPosition.x, gridPosition.y);
 
-    public ref T1 Get(int x, int y) => ref _cells[x - MinX, y - MinY];
-    public ref T1 Get(Vector2Int gridPosition) => ref Get(gridPosition.x, gridPosition.y);
 
-    public void Set(int x, int y, T1 value) => _cells[x - MinX, y - MinY] = value;
-    public void Set(Vector2Int gridPosition, T1 value) => Set(gridPosition.x, gridPosition.y, value);
+    /// <summary>
+    /// 获取格数据引用
+    /// </summary>
+    public ref TCell GetCell(int x, int y) => ref _cells[x - MinX, y - MinY];
+    public ref TCell GetCell(Vector2Int gridPosition) => ref GetCell(gridPosition.x, gridPosition.y);
+
+    /// <summary>
+    /// 获取边数据引用
+    /// </summary>
+    public ref TEdge GetEdge(int x, int y, Direction direction)
+    {
+        var localX = x - MinX;
+        var localY = y - MinY;
+
+        switch (direction)
+        {
+            case Direction.North:
+                return ref _xEdges[localX, localY];
+            case Direction.South:
+                return ref _xEdges[localX + 1, localY];
+            case Direction.West:
+                return ref _yEdges[localX, localY];
+            case Direction.East:
+                return ref _yEdges[localX, localY + 1];
+            default:
+                throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+        }
+    }
+
+    public ref TEdge GetEdge(Vector2Int gridPosition, Direction direction)
+        => ref GetEdge(gridPosition.x, gridPosition.y, direction);
 }
