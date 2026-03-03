fc6fd641264d7043b19961857398126150c75f54
LymoneLM <Lymone@foxmail.com>
2026-03-04 03:17:02 +0800

    feat(Building/LogicalMap): GirdMap 抽象化，使用模版

--- YuanCore/src/Building/LogicalMap/GirdMap.cs
+++ YuanCore/src/Building/LogicalMap/GirdMap.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 
 namespace YuanCore.Building;
 
-public sealed class GridMap
+public sealed class GridMap<T1, T2>
 {
-    private CellData[,] _cells;
-    private EdgeData[,] _horizontalEdges;
-    private EdgeData[,] _verticalEdges;
+    private T1[,] _cells;
+    private T2[,] _horizontalEdges;
+    private T2[,] _verticalEdges;
 
     public int MinX { get; }
     public int MinY { get; }
@@ -25,17 +25,17 @@ public sealed class GridMap
         MinY = minY;
         Width = width;
         Height = height;
-        _cells = new CellData[width, height];
-        _horizontalEdges = new EdgeData[width + 1, height];
-        _verticalEdges = new EdgeData[width, height + 1];
+        _cells = new T1[width, height];
+        _horizontalEdges = new T2[width + 1, height];
+        _verticalEdges = new T2[width, height + 1];
     }
 
     public bool Contains(int x, int y) => x >= MinX && x <= MaxX && y >= MinY && y <= MaxY;
     public bool Contains(Vector2Int girdPosition) => Contains(girdPosition.x, girdPosition.y);
 
-    public ref CellData Get(int x, int y) => ref _cells[x - MinX, y - MinY];
-    public ref CellData Get(Vector2Int gridPosition) => ref Get(gridPosition.x, gridPosition.y);
+    public ref T1 Get(int x, int y) => ref _cells[x - MinX, y - MinY];
+    public ref T1 Get(Vector2Int gridPosition) => ref Get(gridPosition.x, gridPosition.y);
 
-    public void Set(int x, int y, CellData value) => _cells[x - MinX, y - MinY] = value;
-    public void Set(Vector2Int gridPosition, CellData value) => Set(gridPosition.x, gridPosition.y, value);
+    public void Set(int x, int y, T1 value) => _cells[x - MinX, y - MinY] = value;
+    public void Set(Vector2Int gridPosition, T1 value) => Set(gridPosition.x, gridPosition.y, value);
 }
