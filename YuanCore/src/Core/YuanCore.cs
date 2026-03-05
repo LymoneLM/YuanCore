531ad53b8288465c580cbfe3d37faafa7ffda8a3
LymoneLM <Lymone@foxmail.com>
2026-03-05 20:51:24 +0800

    feat(Core): 添加插件入口，引用Json的Nuget包

---
+++ YuanCore/src/Core/YuanCore.cs
@@ -0,0 +1,11 @@
+﻿using BepInEx;
+
+namespace YuanCore.Core;
+
+[BepInPlugin(MODGUID, MODNAME, VERSION)]
+public class YuanCore : BaseUnityPlugin
+{
+    public const string MODNAME = "YuanCore";
+    public const string MODGUID = "cc.lymone.HoL." + MODNAME;
+    public const string VERSION = "0.1.0";
+}
