1b2c49875af9b218552d0c01dc5559b4b78a63ab
LymoneLM <Lymone@foxmail.com>
2026-03-09 22:40:11 +0800

    feat(Core): 重命名插件类名为YuanCorePlugin，引入内部静态Logger

--- YuanCore/src/Core/YuanCore.cs
+++ YuanCore/src/Core/YuanCore.cs
@@ -1,11 +1,19 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
 
 namespace YuanCore.Core;
 
 [BepInPlugin(MODGUID, MODNAME, VERSION)]
-public class YuanCore : BaseUnityPlugin
+public class YuanCorePlugin : BaseUnityPlugin
 {
     public const string MODNAME = "YuanCore";
     public const string MODGUID = "cc.lymone.HoL." + MODNAME;
     public const string VERSION = "0.1.0";
+
+    internal new static ManualLogSource Logger;
+
+    public void Awake()
+    {
+        Logger = base.Logger;
+    }
 }
