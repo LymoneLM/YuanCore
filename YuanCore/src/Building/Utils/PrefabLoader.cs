6b353ef7750cdfe5400e773389edc6fd1426a24d
LymoneLM <Lymone@foxmail.com>
2026-04-04 07:06:58 +0800

    feat(Building/Utils): PrefabLoader新增地图加载和建筑加载，涉及部分一刀切改动后续需要修改

--- YuanCore/src/Building/Utils/PrefabLoader.cs
+++ YuanCore/src/Building/Utils/PrefabLoader.cs
@@ -1,12 +1,94 @@
 ﻿// 这是一个兼容性工具，加载原版预制体并修改为YuanCore需求的结构
 
+using System;
 using UnityEngine;
 using YuanCore.Core;
+using Object = UnityEngine.Object;
 
 namespace YuanCore.Building;
 
 public static class PrefabLoader
 {
+    public static T LoadAsBackMap<T>(string path) where T : Object
+    {
+        var asset = Resources.Load<T>(path);
+        if (asset == null)
+            throw new NullReferenceException($"PrefabLoader: Load failed, path = {path}");
+
+        if (asset is not GameObject prefab)
+            return asset;
+
+        if (path.IndexOf("/l/", StringComparison.Ordinal) != -1 || path.IndexOf("/z/", StringComparison.Ordinal) != -1)
+        {
+            var ts = prefab.transform.Find("AddClickBack");
+            for (var i = 0; i < ts.childCount; ++i)
+            {
+                RemoveAllScripts(ts.GetChild(i).gameObject);
+            }
+
+            ts.gameObject.name = "AllBack";
+            ts = ts.Find("AllKuai");
+            for (var i = 0; i < ts.childCount; ++i)
+            {
+                RemoveAllScripts(ts.GetChild(i).gameObject);
+            }
+        }
+        else
+        {
+            var obj = prefab.transform.Find("AddClickBack").gameObject;
+            if (obj != null)
+                Object.DestroyImmediate(obj);
+        }
+
+        string[] arr = ["BuildPosiGet", "AllZuDang", "BuildTip"];
+        foreach (var name in arr)
+        {
+            var obj = prefab.transform.Find(name).gameObject;
+            if (obj != null)
+                Object.DestroyImmediate(obj);
+        }
+
+        RemoveAllScripts(prefab);
+
+        return asset;
+    }
+
+    public static T LoadAsBuilding<T>(string path) where T : Object
+    {
+        var asset = Resources.Load<T>(path);
+        if (asset == null)
+            throw new NullReferenceException($"PrefabLoader: Load failed, path = {path}");
+
+        if (asset is not GameObject prefab || prefab.GetComponent<BuildingView>() != null)
+            return asset;
+
+        // UI/AllDrag
+        var allDrag = prefab.transform.Find("UI/AllDrag");
+        if (allDrag == null)
+            throw new NullReferenceException($"PrefabLoader: prefab '{prefab.name}' does not contain path 'UI/AllDrag'");
+
+        for (var i = 0; i < allDrag.childCount; i++)
+        {
+            var go = allDrag.GetChild(i).gameObject;
+            RemoveRigidbodies(go);
+            RemoveColliders(go);
+            RemoveAllScripts(go);
+        }
+
+        string[] arr = ["UI/TouMing", "UIBT", "LineCollider", "RangeColliderA", "RangeColliderB", "NPC", "OutQMember"];
+        foreach (var name in arr)
+        {
+            var obj = prefab.transform.Find(name).gameObject;
+            if (obj != null)
+                Object.DestroyImmediate(obj);
+        }
+
+        RemoveAllScripts(prefab);
+        prefab.AddComponent<BuildingView>();
+
+        return asset;
+    }
+
     /// <summary>
     /// 代理 Resources.Load,对 GameObject 预制体进行预处理：<br/>
     /// 根节点挂载 BuildingPlacementView.cs <br/>
@@ -17,10 +99,7 @@ public static class PrefabLoader
     {
         var asset = Resources.Load<T>(path);
         if (asset == null)
-        {
-            YuanCorePlugin.Logger.LogWarning($"PrefabLoader: Load failed, path = {path}");
-            return null;
-        }
+            throw new NullReferenceException($"PrefabLoader: Load failed, path = {path}");
 
         if (asset is not GameObject prefab || prefab.GetComponent<BuildingPlacementView>() != null)
             return asset;
@@ -28,10 +107,7 @@ public static class PrefabLoader
         // UI/AllDrag
         var allDrag = prefab.transform.Find("UI/AllDrag");
         if (allDrag == null)
-        {
-            YuanCorePlugin.Logger.LogWarning($"PrefabLoader: prefab '{prefab.name}' does not contain path 'UI/AllDrag'");
-            return asset;
-        }
+            throw new NullReferenceException($"PrefabLoader: prefab '{prefab.name}' does not contain path 'UI/AllDrag'");
 
         for (var i = 0; i < allDrag.childCount; i++)
         {
@@ -41,6 +117,7 @@ public static class PrefabLoader
             RemoveAllScripts(go);
         }
 
+        RemoveAllScripts(prefab);
         prefab.AddComponent<BuildingPlacementView>();
 
         return asset;
