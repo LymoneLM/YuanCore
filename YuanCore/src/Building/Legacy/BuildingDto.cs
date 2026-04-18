62f445afbf29b7ecd93297c5906f169520c8e1d8
LymoneLM <Lymone@foxmail.com>
2026-04-19 02:38:26 +0800

    feat(Building/Legacy): 补全建筑Dto和DataAdapter

--- YuanCore/src/Building/Legacy/BuildingDto.cs
+++ YuanCore/src/Building/Legacy/BuildingDto.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 namespace YuanCore.Building;
 
@@ -17,4 +17,37 @@ public class BuildingDtoM : BuildingDto
 {
     public int ServantCount;
     public int MonthlyPaymentLevel;
+}
+
+public class BuildingDtoZ : BuildingDto
+{
+    public float FieldSize;
+    public string PlantTime;
+    public int OutputAmountA;
+    public string NextHarvestTime;
+    public int WorkState;
+    public int OutputAmountB;
+}
+
+public class BuildingDtoS : BuildingDto
+{
+    public int EmployeeCount;
+    public string ShopOwnerID;
+    public bool IsOpen;
+    public int SalaryBase;
+    public int AccumulatedProfit;
+    public string LastUpdateTime;
+    public int YuanbaoSpent;
+}
+
+public class BuildingDtoC : BuildingDto
+{
+    public int XueFengClass;
+    public int TuitionCost;
+    public string LastUpdateTime;
+}
+
+public class BuildingDtoL : BuildingDto
+{
+    public string DaiZangData;
 }
